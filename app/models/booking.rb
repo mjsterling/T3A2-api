27fa442920892fe91e918fe2cd6714c99286033d
Deeon Burgan <deeonburgan@gmail.com>
2021-07-19 12:48:34 +1000

    Edited Bookings model and added validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,5 @@
 class Booking < ApplicationRecord
     belongs_to :room
+    validates_presence_of :first_name, :last_name, :email_address, :phone_number
+    validates :num_adults, :num_children, :num_dogs, numericality: {greater_than_or_equal_to: 0}
 end
